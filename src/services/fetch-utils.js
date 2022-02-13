1631dc87e0b00422bed5f81efe82dff11209fa5e
Alex Blair <bclan11@gmail.com>
2022-02-13 15:40:31 -0800

    fetch-utils

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,61 @@
+import { client, checkError } from './client';
+
+export async function getCocktails() {
+  const response = await client.from('cocktails').select('*');
+
+  return checkError(response);
+}
+
+export async function updateCocktail(id, updatedCocktail) {
+  const response = await client.from('cocktails').update(updatedCocktail).match({ id });
+
+  return checkError(response);
+}
+
+export async function deleteCocktail(id) {
+  const response = await client.from('cocktails').delete().match({ id });
+
+  return checkError(response);
+}
+
+export async function getSingleCocktail(id) {
+  const response = await client.from('cocktails').select('*').match({ id }).singlt();
+
+  return checkError(response);
+}
+
+export async function createCocktail(cocktail) {
+  const response = await client.from('cocktails').insert(cocktail);
+
+  return checkError(response);
+}
+
+export async function getUser() {
+  return client.auth.session();
+}
+
+export async function checkAuth() {
+  const user = await getUser();
+  if (!user) location.replace('../');
+}
+
+export async function redirectIfLoggedIn() {
+  if (await getUser()) {
+    location.replace('./home-page');
+  }
+}
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+  return response.user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+  return response.user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+  return (window.location.href = '/');
+}
